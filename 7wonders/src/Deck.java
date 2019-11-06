02c9de3d57dfb5cc464c3526742116ab2ba38251
slhscs228 <slhscs228@T239138-1001L.slhs.katyisd.org>
2019-11-06 13:37:54 -0600

    deck class almost finished

--- 7wonders/src/Deck.java
+++ 7wonders/src/Deck.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
+import static java.lang.System.*;
 
 public class Deck {
 private ArrayList<Card> ageOne;
@@ -13,6 +13,38 @@ ageOne = a1;
 ageTwo = a2;
 ageThree = a3;
 discard = d;
+}
+public ArrayList<Card> getAgeOne()
+{
+	return ageOne;
+}
+public ArrayList<Card> getAgeTWo()
+{
+	return ageTwo;
+}
+public ArrayList<Card> getAgeThree()
+{
+	return ageThree;
+}
+public ArrayList<Card> getDiscard()
+{
+	return discard;
+}
+public void setAgeOne(ArrayList<Card> a1)
+{
+	ageOne = a1;
+}
+public void setAgeTwo(ArrayList<Card> a2)
+{
+	ageTwo = a2;
+}
+public void setAgeThree(ArrayList<Card> a3)
+{
+	ageThree = a3;
+}
+public void setDiscard(ArrayList<Card> d)
+{
+	discard=d;
 }
 public void addDiscard(Card c)
 {
@@ -45,24 +77,57 @@ public void shuffle(int age)
 {
 	if(age==1)
 	{
-		Random rnd = ThreadLocalRandom.current();
-	    for (int i = ageOne.size()-1; i > 0; i--)
-	    {
-	      int index = rnd.nextInt(i + 1);
-	      // Simple swap
-	      int a = ageOne.get(i);
-	     ageOne.get(index) = ageOne.get(i);
-	      ar[i] = a;
-	    }
+		Random random = new Random();
+
+		// start from end of the list
+		for (int i = ageOne.size() - 1; i >= 1; i--)
+		{
+			// get a random index j such that 0 <= j <= i
+			int j = random.nextInt(i + 1);
+
+			// swap element at i'th position in the list with element at
+			// randomly generated index j
+			Card obj = ageOne.get(i);
+			ageOne.set(i, ageOne.get(j));
+			ageOne.set(j, obj);
+		}
 	}
 	else if(age==2)
 	{
-		
+		Random random = new Random();
+
+		// start from end of the list ageTwo
+		for (int i = ageTwo.size() - 1; i >= 1; i--)
+		{
+			// get a random index j such that 0 <= j <= i
+			int j = random.nextInt(i + 1);
+
+			// swap element at i'th position in the list with element at
+			// randomly generated index j
+			Card obj = ageTwo.get(i);
+			ageTwo.set(i, ageTwo.get(j));
+			ageTwo.set(j, obj);
+		}
 	}
 	else if(age==3)
 	{
-		
+		Random random = new Random();
+		for (int i = ageThree.size() - 1; i >= 1; i--)
+		{
+			// get a random index j such that 0 <= j <= i ageThree
+			int j = random.nextInt(i + 1);
+
+			// swap element at i'th position in the list with element at
+			// randomly generated index j
+			Card obj = ageThree.get(i);
+			ageThree.set(i, ageThree.get(j));
+			ageThree.set(j, obj);
+		}
 	}
+}
+public void readInCards()
+{
+	
 }
  
 }
