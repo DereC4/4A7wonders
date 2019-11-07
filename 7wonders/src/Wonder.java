8441b6cb2383b5c68f4e20da4f995c44d2027dc3
@N1v3x <minecraftdumby@gmail.com>
2019-11-07 13:07:54 -0600

    Wonder

--- 7wonders/src/Wonder.java
+++ 7wonders/src/Wonder.java
@@ -13,23 +13,22 @@ public class Wonder
 			return "VP 3";
 		else if (stage == 3)
 			return "VP 7";
-		
-		if (stage == 3)
+		else //if Stage == 2
 		{
 			if (getName().equals("Colossus of Rhodes"))
 				return "WP 2";
 			else if (getName().equals("Halicarnassus"))
 				return "draw"; //Draw from discard 
 			else if (getName().equals("Lighthouse of Alexandria"))
-				return "resource all"; //Resource of choice
+				return "resourceAll"; //Resource of choice
 			else if (getName().equals("Hanging Gardens of Babylon"))
-				return "science all"; //Provides science of choice
+				return "scienceAll"; //Provides science of choice
 			else if (getName().equals("Great Pyramid of Giza"))
 				return "VP 5";
 			else if (getName().equals("Ephesos"))
 				return "coin 9";
-			else if (getName().equals("Olympia"))
-				return 
+			else //Olympia
+				return "ignoreCost";
 		}
 		
 	}
