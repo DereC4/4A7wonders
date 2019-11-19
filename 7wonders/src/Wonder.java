41b043d557e5b47c8f9fd452f473264699d4ee99
slhscs240 <slhscs240@T239242-1001L.slhs.katyisd.org>
2019-11-19 12:52:54 -0600

    Minor Stuffs

--- 7wonders/src/Wonder.java
+++ 7wonders/src/Wonder.java
@@ -19,11 +19,11 @@ public class Wonder
         else //if Stage == 2
         {
             if (getName().equals("Colossus of Rhodes")) return "WP 2";
-            else if (getName().equals("Halicarnassus")) return "draw"; //Draw from discard 
+            else if (getName().equals("Halicarnassus")) return "drawDiscard"; //Draw from discard 
             else if (getName().equals("Lighthouse of Alexandria")) return "resourceAll"; //Resource of choice
             else if (getName().equals("Hanging Gardens of Babylon")) return "scienceAll"; //Provides science of choice
             else if (getName().equals("Great Pyramid of Giza")) return "VP 5";
-            else if (getName().equals("Ephesos")) return "coin 9";
+            else if (getName().equals("Ephesos")) return "C 9";
             else //Olympia
                 return "ignoreCost";
         }
