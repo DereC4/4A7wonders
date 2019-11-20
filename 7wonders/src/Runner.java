4d17699b0d76e4d7865663cf6b426704883f997f
DereC4 <derex1987@gmail.com>
2019-11-20 13:08:46 -0600

    Runner IOException

--- 7wonders/src/Runner.java
+++ 7wonders/src/Runner.java
@@ -1,6 +1,8 @@
+import java.io.IOException;
+
 public class Runner
 {
-	public static void main(String args[]) 
+	public static void main(String args[]) throws IOException 
 	{
 		GameFrame sven = new GameFrame();
 	}
