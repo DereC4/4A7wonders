e9aa5290680ebe5721e4dbaffefe3689421c2659
Kevin Z <Kevin Z@Sunnda-Lenovo>
2019-12-02 15:34:13 -0600

    Wonder stage costs

--- 7wonders/src/Wonder.java
+++ 7wonders/src/Wonder.java
@@ -47,7 +47,9 @@ public class Wonder
             currentStage = 0;
             product = new Resources("Wood");
         }
+        setCurrentStage(0);
     }
+    
     public static final String[] WONDERS = {
         "Colossus of Rhodos",
         "Halikarnassus",
@@ -57,22 +59,97 @@ public class Wonder
         "Ephesos",
         "Statue of Zeus at Olympia"
     };
+    
     public String wonderEffect(int stage)
     {
         if (stage == 1) return "VP 3";
         else if (stage == 3) return "VP 7";
         else //if Stage == 2
         {
-            if (getName().equals("Colossus of Rhodos")) return "WP 2";
-            else if (getName().equals("Halikarnassus")) return "drawDiscard"; //Draw from discard 
-            else if (getName().equals("Lighthouse of Alexandria")) return "resourceAll"; //Resource of choice
-            else if (getName().equals("Hanging Gardens of Babylon")) return "scienceAll"; //Provides science of choice
-            else if (getName().equals("Great Pyramid of Giza")) return "VP 5";
-            else if (getName().equals("Ephesos")) return "C 9";
+            if (getName().equals("Colossus of Rhodos")) 
+            	return "WP 2";
+            else if (getName().equals("Halikarnassus")) 
+            	return "drawDiscard"; //Draw from discard 
+            else if (getName().equals("Lighthouse of Alexandria")) 
+            	return "resourceAll"; //Resource of choice
+            else if (getName().equals("Hanging Gardens of Babylon")) 
+            	return "scienceAll"; //Provides science of choice
+            else if (getName().equals("Great Pyramid of Giza")) 
+            	return "VP 5";
+            else if (getName().equals("Ephesos")) 
+            	return "C 9";
             else //Olympia
                 return "ignoreCost";
         }
     }
+    
+    public String getCost(int stage)
+    {
+    	if (getName().equals("Colossus of Rhodos")) 
+    	{
+        	if (stage == 1)
+        		return "Wood,Wood";
+        	else if (stage == 2)
+        		return "Clay,Clay,Clay";
+        	else 
+        		return "Ore,Ore,Ore,Ore";
+    	}
+        else if (getName().equals("Halikarnassus")) 
+        {
+        	if (stage == 1)
+        		return "Clay,Clay";
+        	else if (stage == 2)
+        		return "Ore,Ore,Ore";
+        	else
+        		return "Loom,Loom";
+        }
+        else if (getName().equals("Lighthouse of Alexandria")) 
+        {
+        	if (stage == 1)
+        		return "Stone,Stone";
+        	else if (stage == 2)
+        		return "Ore,Ore";
+        	else
+        		return "Glass,Glass";
+        }
+        else if (getName().equals("Hanging Gardens of Babylon")) 
+        {
+        	if (stage == 1)
+        		return "Clay,Clay";
+        	else if (stage == 2)
+        		return "Wood,Wood,Wood";
+        	else 
+        		return "Clay,Clay,Clay,Clay";
+        }
+        else if (getName().equals("Great Pyramid of Giza")) 
+        {
+        	if (stage == 1)
+        		return "Stone,Stone";
+        	else if (stage == 2)
+        		return "Wood,Wood,Wood";
+        	else 
+        		return "Stone,Stone,Stone,Stone";
+        }
+        else if (getName().equals("Ephesos")) 
+        {
+        	if (stage == 1)
+        		return "Stone,Stone";
+        	else if (stage == 2)
+        		return "Wood,Wood";
+        	else 
+        		return "Lit,Lit";
+        }
+        else //Olympia
+        {
+        	if (stage == 1)
+        		return "Wood,Wood";
+        	else if (stage == 2)
+        		return "Stone,Stone";
+        	else
+        		return "Ore,Ore";
+        }
+    }
+    
     public String getName()
     {
         return name;
