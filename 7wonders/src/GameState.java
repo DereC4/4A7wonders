9b67fe8da0cb2fbee3b9665ab05a49463e361e8b
@DereC4 <derex1987@gmail.com>
2019-12-11 19:46:56 -0600

    Made some gamestate methods

--- 7wonders/src/GameState.java
+++ 7wonders/src/GameState.java
@@ -1,14 +1,35 @@
 import java.io.IOException;
+import java.util.ArrayList;
 
-public class GameState {
+public class GameState 
+{
 	private Board boardCopy;
-	public GameState() throws IOException {
+	public GameState() throws IOException 
+	{
 		boardCopy=new Board();
 	}
-	public GameState(Board b) throws IOException{
-		boardCopy=b;
+	public GameState(Board newboard) throws IOException
+	{
+		boardCopy=newboard;
 	}
-	public void setGameState(Board ib) {
-		boardCopy=ib;
+	public void setGameState(Board newboard)
+	{
+		boardCopy=newboard;
+	}
+	public void addtonextplayer(Card c)
+	{
+		boardCopy.getCurrentPlayer().addToHand(c);
+	}
+	public ArrayList <Card> getcurrentplayerhand()
+	{
+		return boardCopy.getCurrentPlayer().getHand();
+	}
+	public Player getboardplayer()
+	{
+		return boardCopy.getCurrentPlayer();
+	}
+	public int getcurrentplayerindex()
+	{
+		return boardCopy.getPlayerList().indexOf(boardCopy.getCurrentTurn());
 	}
 }
